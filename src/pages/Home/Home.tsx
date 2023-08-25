71255e54a957b61e11d188796998b77ffb87a4f2
debojitsaha <saha05debojit@gmail.com>
2023-08-25 14:43:02 +0530

    Homepage completed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,36 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
 import { useGetAllProducts } from "../../api/productServices";
 import ProductCard from "../../components/ProductCard";
 import { T_Products } from "../../interfaces/products";
-import "./home.styles.scss";
 import Banner from "../../components/Banner";
+
+import "./home.styles.scss";
 
 const Home = () => {
   const allProducts = useGetAllProducts();
 
   return (
-    <div className="home">
-      <Banner />
-      {allProducts.data?.map((product: T_Products) => {
-        return (
-          <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))' key={product.id}>
-            <ProductCard {...product} />
-          </SimpleGrid>
-        );
-      })}
-    </div>
+    <>
+      <div style={{ padding: "12px" }}>
+        <Banner />
+        <Heading size="lg" justifySelf={"flex-start"} marginTop="6">
+          Products available at ShopCart!
+        </Heading>
+      </div>
+      <div className="home">
+        {allProducts.data?.map((product: T_Products) => {
+          return (
+            <SimpleGrid
+              spacing={4}
+              templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+              key={product.id}
+            >
+              <ProductCard {...product} />
+            </SimpleGrid>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
